9c3e0f5af788a43a568dd6fef9e8f2e612a0dd56
Paul Chen <paulchenpmc@gmail.com>
2020-03-17 21:32:15 -0600

    Adds multiple cells for editing
    Pressing the plus button will add more text cells for editing.
    Couldn't figure out how to change the font color back to white :(

--- code-collab-app/src/editorpage.js
+++ code-collab-app/src/editorpage.js
@@ -5,42 +5,77 @@ import TextField from '@material-ui/core/TextField';
 import { withStyles } from "@material-ui/core/styles";
 import logo from './codeCollabLogo.png';
 import './App.css';
+import IconButton from '@material-ui/core/IconButton';
+import AddIcon from '@material-ui/icons/Add';
 
 const styles = {
-  root: {
-    background: "transparent"
-  },
-  input: {
-    color: "white"
-  }
+  background: "transparent",
+  color: "white",
 };
 
-function Editorpage(props) {
-  const { classes } = props;
+class Editorpage extends React.Component {
+  constructor(props){
+    super(props);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Link to='/'><img src={logo} className="App-logo" alt="logo"/></Link>
-      </header>
-      <body className="App-editor">
-        <TextField
-            className={classes.root}
-            InputProps={{
-              className: classes.input
-            }}
-            id="outlined-multiline-static"
-            label="Editor"
-            multiline
-            fullWidth
-            fullHeight
-            rows="27"
-            defaultValue='print("Hello World!")'
-            variant="filled"
-        />
-      </body>
-    </div>
-  );
+    this.renderTextbox = this.renderTextbox.bind(this);
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+
+    this.state = {
+      childkey: 0,
+      children: []
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      children: [this.renderTextbox()]
+    });
+  }
+
+  renderTextbox() {
+    let keyvalue = this.state.childkey;
+    this.setState({
+      childkey: this.state.childkey + 1,
+    });
+
+    return (
+      <TextField
+        key={keyvalue}
+        id="outlined-multiline-flexible"
+        label="Editor"
+        multiline
+        fullWidth
+        defaultValue='print("Hello World!")'
+        variant="filled"
+      />
+      );
+  }
+
+  handleButtonClick(e) {
+    e.preventDefault();
+
+    this.setState({
+      children: this.state.children.concat([this.renderTextbox()])
+    });
+  }
+
+  render() {
+    return(
+      <div className="App">
+        <header className="App-header">
+          <Link to='/'><img src={logo} className="App-logo" alt="logo"/></Link>
+        </header>
+        <body className="App-editor">
+          <div className="box-container">
+            {this.state.children.map(child => child)}
+          </div>
+          <IconButton aria-label="add" onClick={this.handleButtonClick}>
+            <AddIcon />
+          </IconButton>
+        </body>
+      </div>
+    );
+  }
 }
 
 export default withStyles(styles)(Editorpage);
