39c74253c0e3c22921d4b99ac5ce0379ad2d8351
Bailey Duncan <bailey.duncan@ibm.com>
2020-04-10 13:50:25 -0600

    Move "Enter session name..." to placeholder text

--- code-collab-app/src/namepopup.js
+++ code-collab-app/src/namepopup.js
@@ -1,13 +1,12 @@
-import React from 'react';  
+import React from 'react';
 import './App.css';
 import { Button } from 'react-bootstrap';
 import { inject, observer } from 'mobx-react';
 
 class NamePopup extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-
-    this.state = {title: 'Enter session name...'};
+    this.state = { title: '' };
   }
 
   handleChange = (e) => {
@@ -20,23 +19,29 @@ class NamePopup extends React.Component {
     e.preventDefault();
     this.props.peer.reset(null);
     this.props.peer.create_new_session(this.state.title);
-    this.props.history.push({pathname: '/editor'});
+    this.props.history.push({ pathname: '/editor' });
   }
 
-  render() {  
-    return (  
-      <div className='popup'>  
-        <div className='popup_new_doc'>  
+  render() {
+    return (
+      <div className='popup'>
+        <div className='popup_new_doc'>
           <h3>New Document</h3>
           <div className="input-name">
-            <input type="text" name="name" value={this.state.title} onChange={e => this.handleChange(e)}></input>
+            <input
+              type="text"
+              name="name"
+              placeholder='Enter session name...'
+              value={this.state.title}
+              onChange={e => this.handleChange(e)}
+            ></input>
           </div>
           <Button onClick={(e) => this.handleNewSession(e)} variant='dark'>Create</Button>{'  '}
           <Button onClick={this.props.closePopup} variant="dark">Close</Button>
-        </div>  
+        </div>
       </div>
-    );  
-  }  
-}  
+    );
+  }
+}
 
 export default inject('peer_data')(observer(NamePopup));
